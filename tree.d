f4eb33138776b401f3cf40d49e0d1bcc5a3c2818
jin <nin-jin@ya.ru>
2015-01-14 00:37:54 +0300

    initial implementation

---
+++ tree.d
@@ -0,0 +1,119 @@
+module jin.tree;
+
+import std.string;
+import std.conv;
+import std.outbuffer;
+
+class Tree {
+
+	string name;
+	private string _value;
+	size_t[2] position;
+	Tree[] childs;
+
+	this( string name = "" , string value = "" , Tree[] childs = [] , size_t[2] position = [0,0] ) {
+		this.name = name;
+		this._value = value;
+		this.childs = childs;
+		this.position = position;
+	}
+
+	static parse( string input ) {
+		auto root = new Tree;
+		Tree[] stack = [ root ];
+
+		uint row = 1;
+		uint col = 1;
+
+		Tree last = root;
+		while( input.length ) {
+			auto name = munch( input , "^ \t\n=" );
+			if( name.length ) {
+				auto next = new Tree( name , "" , [] , [ row , col ] );
+				col += name.length;
+				last.childs ~= next;
+				last = next;
+				col += munch( input , " " ).length;
+			} else {
+				if( input[0] == '=' ) {
+					auto value = munch( input , "^\n" )[1..$];
+					auto next = new Tree( "" , value , [] , [ row , col ] );
+					last.childs ~= next;
+					last = next;
+				}
+				row += munch( input , "\n" ).length;
+				auto indent = munch( input , "\t" );
+				col = indent.length;
+				stack ~= last;
+				stack.length = indent.length + 1;
+				last = stack[$-1];
+			}
+		}
+
+		return root;
+	}
+
+	OutputType pipe( OutputType )( OutputType output , string prefix = "" ) {
+		if( this.name.length ) {
+			if( !prefix.length ) {
+				prefix = "\t";
+			}
+			output.write( this.name ~ " " );
+			if( this.childs.length == 1 ) {
+				this.childs[0].pipe( output , prefix );
+				return output;
+			}
+			output.write( "\n" );
+		} else if( this._value.length || prefix.length ) {
+			output.write( "=" ~ this._value ~ "\n" );
+		}
+		foreach( Tree child ; this.childs ) {
+			output.write( prefix );
+			child.pipe( output , prefix ~ "\t" );
+		}
+		return output;
+	}
+
+	Tree select( string path ) {
+		Tree[] next = [ this ];
+		foreach( string name ; path.split(" ") ) {
+			if( !next.length ) break;
+			Tree[] prev = next;
+			next = [];
+			foreach( Tree item ; prev ) {
+				foreach( Tree child ; item.childs ) {
+					if( child.name == name ) {
+						next ~= child;
+					}
+				}
+			}
+		}
+		return new Tree( "" , "" , next );
+	}
+
+	override string toString() {
+		OutBuffer buf = new OutBuffer;
+		this.pipe( buf );
+		return buf.toString();
+	}
+
+	T value( T = string )() {
+		string[] values;
+		foreach( Tree child ; this.childs ) {
+			if( !child.name.length ) {
+				values ~= child.value;
+			}
+		}
+		return to!T( this._value ~ values.join( "\n" ) );
+	}
+
+	Tree opIndex( size_t index ) {
+		return this.childs[ index ];
+	}
+
+	size_t length( ) {
+		return this.childs.length;
+	}
+
+}
+
